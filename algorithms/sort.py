27b9d53033a8a0e2b9f33dbb7dd0c48084727ca8
danphenderson <danphenderson@gmail.com>
2024-01-17 14:03:02 -0800

    Todays sorting alg is merge sort.

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -16,8 +16,43 @@ def bubble_sort(elements:list) -> list:
 def insertion_sort(elements:list) -> list:
     ...
 
+
+def merge(left: list, right:list):
+    """Merge two sorted lists into a single sorted list."""
+    merged = []
+    i = j = 0
+
+    # Traverse both lists and insert smaller of both elements in 'merged'
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    # Once we run out of elements in either list, append the remaining elements
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged    
+
+
 def merge_sort(elements:list) -> list:
-    ...
+    """O(N*logN) runtime. Divide and conquer problem."""
+    size = len(elements)
+
+    # Base Case: an array of zero or one elements is sorted, by definition
+    if size < 2:
+        return elements
+    
+    # Recursive Step
+    left = merge_sort(elements[:size//2])
+    right = merge_sort(elements[size//2:])
+    
+    # Merge the now-sorted sublists
+    return merge(left, right)
+
 
 def quick_sort(elements:list) -> list:
     ...
